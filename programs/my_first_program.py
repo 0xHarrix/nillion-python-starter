c38b8c787a11a424d607d454e32b20821494f1bd
Harris Samuel <132925337+0xHarrix@users.noreply.github.com>
2024-06-30 23:55:55 +0530

    Update my_first_program.py

--- programs/my_first_program.py
+++ programs/my_first_program.py
@@ -1,14 +1,36 @@
 from nada_dsl import *
 
 def nada_main():
-    party1 = Party(name="Party1")
-    my_int1 = SecretInteger(Input(name="my_int1", party=party1))
-    my_int2 = SecretInteger(Input(name="my_int2", party=party1))
+    # Define magical creatures as parties
+    unicorn = Party(name="Unicorn")
+    dragon = Party(name="Dragon")
+    phoenix = Party(name="Phoenix")
+    mermaid = Party(name="Mermaid")
+    griffin = Party(name="Griffin")
 
-    # Perform computation
-    result = my_int1 + my_int2  # Example: Addition of inputs
+    # Each creature votes for their favorite battle move (represented as integers)
+    vote_unicorn = SecretInteger(Input(name="vote_unicorn_move", party=unicorn))
+    vote_dragon = SecretInteger(Input(name="vote_dragon_move", party=dragon))
+    vote_phoenix = SecretInteger(Input(name="vote_phoenix_move", party=phoenix))
+    vote_mermaid = SecretInteger(Input(name="vote_mermaid_move", party=mermaid))
+    vote_griffin = SecretInteger(Input(name="vote_griffin_move", party=griffin))
+
+    # Battle moves represented by integers 1, 2, 3, 4, 5
+    # 1 = Magic Blast, 2 = Fire Breath, 3 = Healing Light, 4 = Water Wave, 5 = Lightning Strike
+    magic_blast_votes = (vote_unicorn == 1) + (vote_dragon == 1) + (vote_phoenix == 1) + (vote_mermaid == 1) + (vote_griffin == 1)
+    fire_breath_votes = (vote_unicorn == 2) + (vote_dragon == 2) + (vote_phoenix == 2) + (vote_mermaid == 2) + (vote_griffin == 2)
+    healing_light_votes = (vote_unicorn == 3) + (vote_dragon == 3) + (vote_phoenix == 3) + (vote_mermaid == 3) + (vote_griffin == 3)
+    water_wave_votes = (vote_unicorn == 4) + (vote_dragon == 4) + (vote_phoenix == 4) + (vote_mermaid == 4) + (vote_griffin == 4)
+    lightning_strike_votes = (vote_unicorn == 5) + (vote_dragon == 5) + (vote_phoenix == 5) + (vote_mermaid == 5) + (vote_griffin == 5)
+
+    # Determine the most popular battle move
+    most_popular_move = (magic_blast_votes > fire_breath_votes) & (magic_blast_votes > healing_light_votes) & (magic_blast_votes > water_wave_votes) & (magic_blast_votes > lightning_strike_votes) * 1 + \
+                        (fire_breath_votes > magic_blast_votes) & (fire_breath_votes > healing_light_votes) & (fire_breath_votes > water_wave_votes) & (fire_breath_votes > lightning_strike_votes) * 2 + \
+                        (healing_light_votes > magic_blast_votes) & (healing_light_votes > fire_breath_votes) & (healing_light_votes > water_wave_votes) & (healing_light_votes > lightning_strike_votes) * 3 + \
+                        (water_wave_votes > magic_blast_votes) & (water_wave_votes > fire_breath_votes) & (water_wave_votes > healing_light_votes) & (water_wave_votes > lightning_strike_votes) * 4 + \
+                        (lightning_strike_votes > magic_blast_votes) & (lightning_strike_votes > fire_breath_votes) & (lightning_strike_votes > healing_light_votes) & (lightning_strike_votes > water_wave_votes) * 5
 
     # Define the output
-    output = Output(result, "my_output", party1)
+    output = Output(most_popular_move, "most_popular_battle_move", unicorn)
 
     return [output]
